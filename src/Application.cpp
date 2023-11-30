2862b215921d6df01a5bf0cb0a630eec88d0e57a
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-11-30 13:14:05 -0500

    Cleanup

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,7 +2,9 @@
 
 using namespace star; 
 
-Application::Application(star::StarScene& scene) : StarApplication(scene)
+Application::Application(star::StarScene& scene) : StarApplication(scene) {}
+
+void Application::Load()
 {
     this->camera.setPosition(glm::vec3{ 2.0, 1.0f, 3.0f });
     auto camPosition = this->camera.getPosition();
@@ -12,50 +14,34 @@ Application::Application(star::StarScene& scene) : StarApplication(scene)
     {
         auto lionPath = star::ConfigFile::getSetting(star::Config_Settings::mediadirectory) + "models/lion-statue/source/rapid.obj";
         auto materialsPath = mediaDirectoryPath + "models/lion-statue/source";
-        
+
         auto lion = DispNormObj::New(lionPath);
-        const int width = 15; 
-        const float mvmt = 0.3f; 
-        const float texDispAmt = float(1.0f / ((width-1)*2));  
+        const int width = 15;
+        const float mvmt = 0.3f;
+        const float texDispAmt = float(1.0f / ((width - 1) * 2));
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < width; j++) {
                 auto& lion_f = lion->createInstance();
-                DispNormObjInstance& lion_ptr = static_cast<DispNormObjInstance&>(lion_f); 
+                DispNormObjInstance& lion_ptr = static_cast<DispNormObjInstance&>(lion_f);
                 auto amt = texDispAmt * (i + j);
                 lion_ptr.textureDisplacement = lion_ptr.textureDisplacement * amt;
                 lion_f.setScale(glm::vec3{ 0.01f, 0.01f, 0.01f });
                 lion_f.setPosition(glm::vec3{ 0.0, 0.0, 0.0 });
                 lion_f.rotateGlobal(star::Type::Axis::x, -90);
-                lion_f.moveRelative(glm::vec3{ mvmt * i, 0.0, mvmt * j});
+                lion_f.moveRelative(glm::vec3{ mvmt * i, 0.0, mvmt * j });
             }
         }
 
-        this->scene.add(std::move(lion)); 
+        this->scene.add(std::move(lion));
         this->scene.add(std::make_unique<star::Light>(star::Type::Light::point, glm::vec3{ 0, 1, 0 }));
-        this->scene.add(std::make_unique<star::Light>(star::Type::Light::point, glm::vec3{ 2, 1, 2 }, glm::vec4{1.0, 0.0, 0.0, 1.0},
-            glm::vec4{ 1.0, 0.0, 0.0, 1.0 }, glm::vec4{1.0, 0.0, 0.0, 1.0}));
+        this->scene.add(std::make_unique<star::Light>(star::Type::Light::point, glm::vec3{ 2, 1, 2 }, glm::vec4{ 1.0, 0.0, 0.0, 1.0 },
+            glm::vec4{ 1.0, 0.0, 0.0, 1.0 }, glm::vec4{ 1.0, 0.0, 0.0, 1.0 }));
         this->scene.add(std::make_unique<star::Light>(star::Type::Light::point, glm::vec3{ 4, 1, 4 }, glm::vec4{ 0.0, 1.0, 0.0, 1.0 },
             glm::vec4{ 0.0, 1.0, 0.0, 1.0 }, glm::vec4{ 0.0, 1.0, 0.0, 1.0 }));
         this->scene.add(std::make_unique<star::Light>(star::Type::Light::point, glm::vec3{ 6, 1, 6 }, glm::vec4{ 0.0, 0.0, 1.0, 1.0 },
             glm::vec4{ 0.0, 0.0, 1.0, 1.0 }, glm::vec4{ 0.0, 0.0, 1.0, 1.0 }));
-        this->scene.add(std::make_unique<star::Light>(star::Type::Light::directional, glm::vec3{10, 10, 10}));
+        this->scene.add(std::make_unique<star::Light>(star::Type::Light::directional, glm::vec3{ 10, 10, 10 }));
     }
-
-    std::cout << "App Controls" << std::endl;
-    std::cout << "Press M to switch lights off and on in order" << std::endl;
-    std::cout << "Right Arrow: rotate sun light clockwise" << std::endl;
-    std::cout << "Left Arrow: rotate sun light counter clockwise" << std::endl;
-    std::cout << "Up Arrow: increase diameter of spot light" << std::endl;
-    std::cout << "Down Arrow: decrease diameter of spot light" << std::endl;
-    std::cout << "U: Increase outer diam of spot light" << std::endl;
-    std::cout << "Y: Decrease outer diam of spot light" << std::endl;
-    std::cout << "J: Increase inner diam of spot light" << std::endl;
-    std::cout << "H: Decrease inner diam of spot light" << std::endl;
-    std::cout << "Z: Enable/Disable sun" << std::endl;
-}
-
-void Application::Load()
-{
 }
 
 void Application::onKeyPress(int key, int scancode, int mods)
